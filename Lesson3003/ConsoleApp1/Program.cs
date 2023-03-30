6a56346131e0079494dba9ed7baca7d92d6a3e23
Aleksey Shishkarev <whiteverse@gmail.com>
2023-03-30 15:44:48 +0300

    Created new solution of metod

---
+++ Lesson3003/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           void Increment (ref int n)
+            {
+                n++;
+                Console.WriteLine($"Число в методе Increment: {n}");
+            }
+            int number = 5;
+            Console.WriteLine($"Число до метода Increment: {number}");
+
+            Increment(ref number);
+            Console.WriteLine($"Число после метода Increment: {number}");
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
